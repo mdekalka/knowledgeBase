9289641228869883dfb3b7844f45cdfd51a3a7c3
Aleh_Isakau <Aleh_Isakau@epam.com>
2014-08-29 18:03:33 +0300

    add simple paging

--- js/main.js
+++ js/main.js
@@ -9,16 +9,43 @@
         ================================================================*/
         var dynamicContent = document.querySelector('.code-container-dynamic'),
             menuBtns = document.querySelectorAll('.btn-main'),
-            mainNavLists = document.querySelectorAll('.main-nav li');
+            mainNavLists = document.querySelectorAll('.main-nav li'),
+            loader = document.querySelector('.loader');
 
         /*================================================================
                             >>> HELPERS FUNCTION <<<  
           ================================================================*/
         function removeClass(element, className) {
+            var reg = new RegExp(className, "g");
             for (var i = 0, l = element.length; i < l; i++) {
-                element[i].className = element[i].className.replace(/\b\+'className'\+\b/,'');
+                element[i].className = element[i].className.replace(reg,'');
             };
         };
+
+        function ajaxLoad(url) {
+            var xhr = new XMLHttpRequest(),
+                data;
+            xhr.onreadystatechange = ajaxState;
+
+            function ajaxState() {
+                if (xhr.readyState === 4) {
+                    var status = xhr.status;
+                    if (status >= 200 && status < 300 || status === 304) {
+                        data = xhr.responseText;
+                        addLoadedPage(data);
+                        Prism.highlightAll(data);
+                    } else {
+                        console.log('Error');
+                    };
+                };
+            };
+            xhr.open('GET', url, true);
+            xhr.send();
+        };
+
+        function addLoadedPage(content) {
+            dynamicContent.innerHTML = content;
+        }
 
         /*================================================================
                             >>> HANDLERS <<<  
@@ -36,12 +63,15 @@
 
         function callBackListener() {
             var self = this;
-            console.log(self.dataset.attr);
+
             if ( !self.parentNode.className.match(/(?:^|\s)active(?!\S)/) ) {
                 removeClass(mainNavLists, 'active');
                 self.parentNode.className += " active";
-            } else {
-                // self.parentNode.className.replace(/\bactive\b/,'');
+            };
+
+            if (self.dataset.attr = "js") {
+                var getPage = ajaxLoad('pages/JavaScript/javascript.html');
+
             };
         };
 
